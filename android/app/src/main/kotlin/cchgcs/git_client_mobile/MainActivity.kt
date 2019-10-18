69591b03b1b8ace3273c562c70e5febcdc1b5f21
Lucas Wolf <lucasgiovanew@gmail.com>
2019-10-18 17:04:28 -0300

    Feat: Empty application project

---
+++ android/app/src/main/kotlin/cchgcs/git_client_mobile/MainActivity.kt
@@ -0,0 +1,13 @@
+package cchgcs.git_client_mobile
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
