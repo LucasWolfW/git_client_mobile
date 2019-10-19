d7df9e548a0a9dc475f74f72f646c93106a142b4
Lucas Wolf <lucasgiovanew@gmail.com>
2019-10-19 00:02:04 -0300

    Feat: Adicionado imagem para aba lateral

--- lib/main.dart
+++ lib/main.dart
@@ -56,62 +56,35 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: RichText(
-                text: TextSpan(
-                  style: DefaultTextStyle.of(context).style,
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'Git Client Mobile \n',
-                      style: TextStyle(
-                        decoration: TextDecoration.none,
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'Your Git Solution For Mobile Application.\n',
-                      style: TextStyle(
-                        decoration: TextDecoration.none,
-                        color: Colors.white,
-                        fontSize: 10,
-                      ),
-                    ),
-                  ],
+        child: Container(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: ListView(),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/AppBarLogo.png'),
+                      fit: BoxFit.fitWidth),
                 ),
               ),
-              decoration: BoxDecoration(
-                color: primaryDarkGrey,
+              ListTile(
+                title: Text('Projects'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
               ),
-            ),
-            ListTile(
-              title: Text('Projects'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Settings'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
+              ListTile(
+                title: Text('Settings'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+          color: primaryDarkGrey,
         ),
       ),
     );
